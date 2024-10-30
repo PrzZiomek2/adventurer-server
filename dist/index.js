36089d5414a9aeaf9aa43cf1a18cfee9eef5abf7
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 <pzz64056@gmail.com>
2024-10-30 07:56:06 +0100

    count places clicks and save to db - proof of concept

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.client = void 0;
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const cors_1 = __importDefault(require("cors"));
+const compression_1 = __importDefault(require("compression"));
+const pg_1 = require("pg");
+require("dotenv/config");
+const routes_1 = __importDefault(require("./routes"));
 const app = (0, express_1.default)();
 const port = 5000;
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.use((0, cors_1.default)());
+app.use(express_1.default.json({ limit: "50mb" }));
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
+app.use((0, compression_1.default)({ threshold: 512 }));
+app.use((_, res, next) => {
+    res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
 });
+exports.client = new pg_1.Client({
+    user: "postgres",
+    password: process.env.PG_DB_PASSWORD,
+    host: "localhost",
+    port: 5432,
+    database: process.env.PG_DB_NAME,
+});
+exports.client.connect().then(() => {
+    console.log("pg database connected");
+});
+app.use(routes_1.default);
 app.listen(port, () => {
     console.log(`server listening at http://localhost:${port}`);
 });
---
+++ src/controllers/place.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from "express";
+import { client } from "..";
+
+interface Place {
+  name: string;
+  id: string;
+  click_location: string;
+}
+
+export const placeClicksController = async (req: Request, res: Response) => {
+  const { name, id, click_location } = req.body.place as Place;
+
+  const createTableQuery = `
+        CREATE TABLE IF NOT EXISTS clicks (
+            name VARCHAR(200) PRIMARY KEY,
+            id VARCHAR(100),
+            count_click_map INTEGER DEFAULT 0,
+            count_click_details INTEGER DEFAULT 0
+        )
+    `;
+  const insertDataQuery = `
+        INSERT INTO clicks (name, id, count_click_map, count_click_details) 
+        VALUES (
+            $1, 
+            $2, 
+            CASE WHEN $3 = 'map' THEN 1 ELSE 0 END, 
+            CASE WHEN $3 = 'details' THEN 1 ELSE 0 END
+            
+        )
+         ON CONFLICT (name)
+         DO UPDATE SET
+            count_click_map = clicks.count_click_map + 
+                (CASE WHEN EXCLUDED.count_click_map = 1 THEN 1 ELSE 0 END),
+            count_click_details = clicks.count_click_details + 
+                (CASE WHEN EXCLUDED.count_click_details = 1 THEN 1 ELSE 0 END),
+            id = EXCLUDED.id
+    `;
+
+  try {
+    if (!name || !id || !click_location) {
+      throw new Error("All fields are required");
+    }
+
+    await client.query(createTableQuery);
+    await client.query(insertDataQuery, [name, id, click_location]);
+
+    res.status(200).json({
+      message: "Data inserted successfully",
+    });
+  } catch (error) {
+    console.log({ error });
+
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};

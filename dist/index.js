df4277e1b5a7f136342347ac3d5e7c44fee0c199
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 <pzz64056@gmail.com>
2024-11-01 11:55:55 +0100

    separate postgre db logic from kafka service

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -11,6 +20,7 @@ const compression_1 = __importDefault(require("compression"));
 const pg_1 = require("pg");
 require("dotenv/config");
 const routes_1 = __importDefault(require("./routes"));
+const postgreDb_1 = require("./services/postgreDb");
 const kafka_1 = require("./services/kafka");
 const app = (0, express_1.default)();
 const port = 5000;
@@ -31,12 +41,32 @@ exports.pgData = {
     port: 5432,
     database: process.env.PG_DB_NAME,
 };
+const kafkaConfig = {
+    clientId: "node-click-consumer",
+    brokers: ["localhost:9092"],
+    connectionTimeout: 3000,
+    retry: {
+        retries: 5,
+    },
+};
 exports.client = new pg_1.Client(exports.pgData);
 exports.client.connect().then(() => {
     console.log("pg database connected");
 });
 app.use(routes_1.default);
-(0, kafka_1.runKafkaConsumer)().catch(console.error);
+const runKafkaConsumer = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const dbService = new postgreDb_1.DbService(exports.pgData);
+        const kafkaService = new kafka_1.KafkaService(kafkaConfig, dbService);
+        yield kafkaService.connect();
+        yield kafkaService.subscribe("click-events");
+        yield kafkaService.run();
+    }
+    catch (err) {
+        console.log("Error while running kafka consumer:", err);
+    }
+});
+runKafkaConsumer();
 app.listen(port, () => {
     console.log(`server listening at http://localhost:${port}`);
 });
--- src/services/kafka.ts
+++ src/services/kafka.ts
@@ -1,25 +1,5 @@
-import { Kafka } from "kafkajs";
-import { Pool } from "pg";
-import "dotenv/config";
-
-const pgData = {
-  user: "postgres",
-  password: process.env.PG_DB_PASSWORD,
-  host: "localhost",
-  port: 5432,
-  database: process.env.PG_DB_NAME,
-};
-
-const kafka = new Kafka({
-  clientId: "node-click-consumer",
-  brokers: ["localhost:9092"],
-  connectionTimeout: 3000,
-  retry: {
-    retries: 5,
-  },
-});
-
-const consumer = kafka.consumer({ groupId: "click-group" });
+import { Kafka, KafkaConfig } from "kafkajs";
+import { DbService } from "./postgreDb";
 
 interface Place {
   name: string;
@@ -27,70 +7,51 @@ interface Place {
   click_location: string;
 }
 
-let clickBatch: Place[] = [];
+export class KafkaService {
+  private kafka;
+  private consumer;
+  private dbService: DbService;
+  private clickBatch: Place[] = [];
 
-const pool = new Pool(pgData);
-
-async function saveBatchToDatabase() {
-  if (!clickBatch.length) return;
-
-  const client = await pool.connect();
-  try {
-    const createTableQuery = `
-          CREATE TABLE IF NOT EXISTS clicks (
-            name VARCHAR(200) PRIMARY KEY,
-            id VARCHAR(100),
-            count_click_map INTEGER DEFAULT 0,
-            count_click_details INTEGER DEFAULT 0
-          );
-        `;
-    await client.query(createTableQuery);
-
-    const insertDataQuery = `
-          INSERT INTO clicks (name, id, count_click_map, count_click_details) 
-          VALUES (
-            $1, 
-            $2, 
-            CASE WHEN $3 = 'map' THEN 1 ELSE 0 END, 
-            CASE WHEN $3 = 'details' THEN 1 ELSE 0 END
-          )
-          ON CONFLICT (name)
-          DO UPDATE SET
-            count_click_map = clicks.count_click_map + 
-                (CASE WHEN EXCLUDED.count_click_map = 1 THEN 1 ELSE 0 END),
-            count_click_details = clicks.count_click_details + 
-                (CASE WHEN EXCLUDED.count_click_details = 1 THEN 1 ELSE 0 END),
-            id = EXCLUDED.id;
-        `;
-
-    for (const data of clickBatch) {
-      const { name, id, click_location } = data;
-      await client.query(insertDataQuery, [name, id, click_location]);
-    }
-    clickBatch = [];
-  } catch (error) {
-    console.error("Error while saving batch to database:", error);
-  } finally {
-    client.release();
+  constructor(kafkaConfig: KafkaConfig, dbService: DbService) {
+    this.kafka = new Kafka(kafkaConfig);
+    this.consumer = this.kafka.consumer({ groupId: "click-group" });
+    this.dbService = dbService;
   }
-}
 
-export async function runKafkaConsumer() {
-  await consumer.connect();
-  await consumer.subscribe({ topic: "click-events", fromBeginning: true });
+  async connect() {
+    await this.consumer.connect();
+    await this.dbService.init();
+  }
 
-  await consumer.run({
-    eachMessage: async ({ message }) => {
-      if (!message.value) return;
+  async subscribe(topic: string) {
+    await this.consumer.subscribe({ topic, fromBeginning: true });
+  }
 
-      const clickEvent = JSON.parse(message.value.toString());
-      clickBatch.push(clickEvent);
+  async run() {
+    await this.consumer.run({
+      eachMessage: async ({ message }) => {
+        if (!message.value) return;
 
-      if (clickBatch.length >= 50) {
-        await saveBatchToDatabase();
+        const clickEvent: Place = JSON.parse(message.value.toString());
+        this.clickBatch.push(clickEvent);
+
+        if (this.clickBatch.length >= 50) {
+          await this.dbService.insertClicksData(this.clickBatch);
+          this.clickBatch = [];
+        }
+      },
+    });
+
+    setInterval(async () => {
+      if (this.clickBatch.length) {
+        await this.dbService.insertClicksData(this.clickBatch);
+        this.clickBatch = [];
       }
-    },
-  });
+    }, 5000);
+  }
 
-  setInterval(saveBatchToDatabase, 5000);
+  async disconnect() {
+    await this.consumer.disconnect();
+  }
 }
